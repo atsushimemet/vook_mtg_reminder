fa6f923469bd6a8bded129fc4f18ad76e1a0d420
atsushimemet <noap3b69n@gmail.com>
2023-11-26 10:45:27 +0900

    uddate: to track log & hide channel_id

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import datetime
-import sys
+import logging
 
+from config import channel_id
 from utils import calculate_saturdays, send_slack_message
+
+# ロギングの設定
+logging.basicConfig(
+    filename="slack_messaging.log",
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+)
 
 
 def main():
@@ -16,28 +24,26 @@ def main():
     one_week_before_first = first_saturday - datetime.timedelta(days=7)
     one_week_before_third = third_saturday - datetime.timedelta(days=7)
 
-    # 特定のチャンネルIDを指定
-    channel_id = "C04N08LUDCL"  # randomチャンネルのID
+    # date_str = "test" # テスト用
+    # if today: # テスト用
+    if today == one_week_before_first or today == one_week_before_third:
+        if today == one_week_before_first:
+            date_str = first_saturday.strftime("%m/%d（%a）")
+        else:
+            date_str = third_saturday.strftime("%m/%d（%a）")
 
-    # メッセージの日付部分を動的に設定
-    if today == one_week_before_first:
-        date_str = first_saturday.strftime("%m/%d（%a）")
-    elif today == one_week_before_third:
-        date_str = third_saturday.strftime("%m/%d（%a）")
+        message = f"""テスト
+        {date_str} 隔週mtg開催予定です。
+        場所:〒150-0031 東京都渋谷区桜丘町９−１８ タカシマ桜ケ丘マンション
+        日時: {date_str} 13:00-15:00
+
+        1. 出席可否
+        2. 出席可否問わずアジェンダある方はスレッドにお願いします
+        """
+        send_slack_message(channel_id, message)
+        logging.info(f"Message sent for {date_str}")
     else:
-        sys.exit()
-
-    message = f"""テストなので無視してください
-    {date_str} 隔週mtg開催予定です。
-    場所:〒150-0031 東京都渋谷区桜丘町９−１８ タカシマ桜ケ丘マンション
-    日時: {date_str} 13:00-15:00
-
-    1. 出席可否
-    2. 出席可否問わずアジェンダある方はスレッドにお願いします
-    """
-
-    # メッセージを送信
-    send_slack_message(channel_id, message)
+        logging.info("No message sent today")
 
 
 if __name__ == "__main__":
