e2b3e92bb7e18decf106f4e152907a4ecc32782f
atsushimemet <noap3b69n@gmail.com>
2023-11-26 10:24:41 +0900

    update: message

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def main():
     first_saturday, _ = calculate_saturdays(current_year, current_month)
 
     # 特定のユーザーのDMチャンネルIDを指定
-    user_dm_channel_id = "C04N08LUDCL"
+    channel_id = "C04N08LUDCL"  # random
 
+    message = """テスト
+    11/18（土）隔週mtg開催予定です。
+    場所:〒150-0031 東京都渋谷区桜丘町９−１８ タカシマ桜ケ丘マンション
+    日時: 11/18（土）13:00-15:00
+
+    1. 出席可否
+    2. 出席可否問わずアジェンダある方はスレッドにお願いします
+    """
     # 第一土曜日のアジェンダ募集のメッセージを送信
-    send_slack_message(user_dm_channel_id, "テスト:第一土曜日のアジェンダを募集してください。")
+    send_slack_message(channel_id, message)
 
 
 if __name__ == "__main__":
